3162230b1f89c22fbbdcfa25e20a9bf4c1d536df
Leo CP <leo011093@hotmail.com>
2016-12-09 17:43:08 -0500

    creating the controller -FrontController-

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+    	return view('index');
+    }
+
+    public function contacto()
+    {
+    	return view('contacto');
+    }
+
+    public function resenias()
+    {
+    	return view('resenias');
+    }
+}
