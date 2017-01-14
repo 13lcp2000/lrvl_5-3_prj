6c6cdde9256ab8bf08c702eccc2c58fc8f51806c
Leo CP <leo011093@hotmail.com>
2017-01-13 19:39:24 -0500

    importing some files and creating the construct method

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,14 +3,37 @@
 namespace Cinema\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+
+//use \Cinema\Http\Request;
+use \Cinema\Http\Requests\UserCreateRequest;
+use \Cinema\Http\Requests\UserUpdateRequest;
+//use \Cinema\Http\Controllers\Controller;
+//use \Cinema\Http\Controller;
 use \Cinema\User;
+
+//use Sessions;
+//use Redirect;
 
 use  Illuminate\Support\Facades\Session;
 use  Illuminate\Support\Facades\Redirect;
+use  Illuminate\Routing\Route;
 
 
 class UsuarioController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+        //$this->middleware('admin');
+        //$this->beforeFilter('@find', ['only' => ['edit','update','destroy']]);
+    }
+
+    /*public function find(Route $route){
+        $this->user = User::find($route->getParameter('usuario'));
+        return $this->user;
+    }*/
+
     /**
      * Display a listing of the resource.
      *
@@ -18,8 +41,10 @@ class UsuarioController extends Controller
      */
     public function index()
     {
-        $users = User::All();
+        //$users = User::All();
+        $users = User::paginate(4); //para usar el paginador
         return view('usuario.index', compact('users'));
+
     }
 
     /**
@@ -38,17 +63,20 @@ class UsuarioController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(UserCreateRequest $request)
+    {   
+
+        //$this->validate($request, [
+        //    'name' => 'required|max:255',
+        //    'email' => 'required|email|max:255|unique:users',
+        //    'password' => 'required|min:6|confirmed',
+        //]);
+
         //return "aki stoy";
-        User::create([
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => $request['password'],
-            ]);
+        User::create($request->all());
 
         //return '<strong> usuario registrado </strong>';
-        return redirect('/usuario')->with('message','store');
+        return redirect('/usuario')->with('message','Usuario "'.$request['name'].'" Almacenado Correctamente');
     }
 
     /**
@@ -81,7 +109,7 @@ class UsuarioController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, $id)
     {
         $user = User::find($id);
         $user->fill($request->all());
@@ -99,6 +127,10 @@ class UsuarioController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //User::destroy($id); ya no usaremos esto por que no queremos destriur el recurso...
+        $user = User::find($id);
+        $user->delete();
+        Session::flash('message', 'Usuario Eliminado Correctamente');
+        return Redirect::to('/usuario');
     }
 }
