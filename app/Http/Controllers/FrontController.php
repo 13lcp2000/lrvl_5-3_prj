1969f02107b92f1f73450d861ea8693f55e2d968
Leo CP <leo011093@hotmail.com>
2017-01-13 19:04:22 -0500

    creating the construct method with the middleware

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
+    
+    public function __construct(){
+        $this->middleware('auth',['only' => 'admin']);
+    }
+
     public function index()
     {
     	return view('index');
