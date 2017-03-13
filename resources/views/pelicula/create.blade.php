8212d4e6d995713c4aad0832edc379892140b9aa
Leo CP <leo011093@hotmail.com>
2017-03-12 22:59:56 -0500

    creating the -pelicula_create- blade template

---
+++ resources/views/pelicula/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+	@section('content')
+		@include('alerts.request')
+			<br> 
+		  	{!!Form::open(['route'=>'pelicula.store', 'method'=>'POST','files' => true])!!}
+		  		@include('pelicula.forms.pelicula')
+				{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
+			{!!Form::close()!!}
+	@endsection
